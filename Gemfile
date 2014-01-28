be7485cc7407b2bbbdca91c87fe1decac3a82222
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-01-28 12:23:46 -0800

    Initial install Devise installed

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'devise'
+gem 'minitest'
+gem 'minitest-rails'
+gem 'foundation-rails'
+
+group :test do
+  gem "minitest-rails-capybara"
+  gem "launchy"
+  gem "turn"
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'rails_12factor', group: :production
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qanda::Application.config.secret_key_base = '6092db1054a44c3c2f46ba44d497620c01da4dfcb7e04fcb87788b5a17c6fdb358074219a768f13c1f337e5e24315d22969da681d4279109c1d7d925f6fb6560'
